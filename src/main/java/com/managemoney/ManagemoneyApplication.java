ddd86c77eac0fc722726a87670767eee0cf4f51b
Chinmaya Sahoo <cinshu39@gmail.com>
2025-07-19 23:26:12 +0530

    project change the package

--- src/main/java/com/managemoney/ManagemoneyApplication.java
+++ src/main/java/com/managemoney/ManagemoneyApplication.java
@@ -1,4 +1,4 @@
-package com.managemoney.managemoney;
+package com.managemoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/managemoney/ManagemoneyApplicationTests.java
+++ src/test/java/com/managemoney/ManagemoneyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.managemoney.managemoney;
+package com.managemoney;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
