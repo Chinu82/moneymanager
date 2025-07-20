5aaa70f5378d05fe19171bced873273f39b43237
Chinmaya Sahoo <cinshu39@gmail.com>
2025-07-20 21:19:41 +0530

    feat : for email service

--- src/main/java/com/managemoney/controller/HomeController.java
+++ src/main/java/com/managemoney/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("status")
+@RequestMapping({"/status","/health"})
 public class HomeController {
     @GetMapping("/health")
     public String healthCheck(){
